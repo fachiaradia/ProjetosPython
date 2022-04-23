50771e2f3df0414452e4f5dd3eedf084f44eb486
Fábio Chiaradia <fk.chiaradia@gmail.com>
2022-04-23 13:49:21 -0300

    Initial commit

---
+++ calculadora.py
@@ -0,0 +1,33 @@
+#Título
+print("******CALCULADORA DO FÁBIO******")
+
+#Variáveis
+soma = int("1")
+subtra = int("2")
+multipli = int("3")
+divisao = int("4")
+
+
+#Opções
+print("Escolha uma das operações desejadas:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+#Seleção da operação pelo usuário
+operacao = int(input("Digite operação desejada (1/2/3/4): "))
+
+#Input dos valores pelo usuário
+priNum = int(input("Digite primeiro número: "))
+segNum = int(input("Digite segundo número: "))
+
+if operacao == soma:
+    print("Resultado é: ",priNum+segNum)
+elif operacao == subtra:
+    print("Resultado é: ",priNum-segNum)
+elif operacao == multipli:
+    print("Resultado é: ",priNum*segNum)
+else:
+    print("Resultado é: ",priNum/segNum)
+
